74cd92dbd33a9fbc8f8b5324b8ed0463ad9a14de
PinoyCoder29 <bicojayvee4@gmail.com>
2025-09-23 14:59:09 +0800

    updated MainHeader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import MainHeader from "@/components/layout/main/MainHeader";
 import { ReactNode } from "react";
+import Script from "next/script";
 
-export default function MainLayout({children}: {children: ReactNode}) {
-  return(
-   <html>
-    <body>
-      <MainHeader/>
-      <main>{children}</main>
-    </body>
-   </html>
-  )
+export default function MainLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <MainHeader />
+        <main>{children}</main>
+
+        {/* Bootstrap JS via CDN */}
+        <Script 
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+          strategy="beforeInteractive" 
+        />
+      </body>
+    </html>
+  );
 }
--- src/components/layout/main/MainHeader.tsx
+++ src/components/layout/main/MainHeader.tsx
@@ -1,10 +1,40 @@
 export default function MainHeader() {
-    return(
-        <>
-        <nav className="navbar navbar-expand-md navbar-light bg-light shadow">
+  return(
+    <>
+    <nav className="navbar navbar-expand-md navbar-light bg-light shadow">
+      <div className="container">
+      <h1 className="navbar-brand">Pt_Finder</h1>
+
+          <button className="navbar-toggler"
+           type="button"
+            data-bs-toggle="offcanvas"
+             data-bs-target="#offcanvasScrolling"
+              aria-controls="offcanvasScrolling">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+
+          <div className="offcanvas offcanvas-start" style={{width:'50%'}} data-bs-scroll="true" data-bs-backdrop="false" tabIndex={-1} id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasScrollingLabel">Menu</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body">
+             
             
-        </nav>
-        
-        </>
-    )
+              <ul className="navbar-nav mx-auto">
+                <li className="nav-item"><a href="" className="nav-link">Home</a></li>
+                <li className="nav-item"><a href="" className="nav-link">About</a></li>
+                <li className="nav-item"><a href="" className="nav-link">Contact</a></li>
+              </ul>
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item"><a href="" className="nav-link">Get Started</a></li> 
+                <li className="nav-item"><a href="" className="nav-link">Sign In</a></li>
+              </ul>
+         
+            </div>
+          </div>
+      </div>
+    </nav>
+    </>
+  )
 }

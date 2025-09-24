3757eb69faee11bcdee1642fdad49840118045e9
PinoyCoder29 <bicojayvee4@gmail.com>
2025-09-24 14:48:26 +0800

    add signUp page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MainHeader from "@/components/layout/main/MainHeader";
 import { ReactNode } from "react";
 import Script from "next/script";
+import MainHeader from "@/components/layout/main/MainHeader";
 
-export default function MainLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
         <MainHeader />
         <main>{children}</main>
-        
+
         <Script 
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
           strategy="beforeInteractive" 
--- src/components/layout/main/MainHeader.tsx
+++ src/components/layout/main/MainHeader.tsx
@@ -1,40 +1,80 @@
+import Link from "next/link";
+
 export default function MainHeader() {
-  return(
+  return (
     <>
-    <nav className="navbar navbar-expand-md navbar-light bg-light shadow">
-      <div className="container">
-      <h1 className="navbar-brand">Pt_Finder</h1>
+      <nav className="navbar navbar-expand-md navbar-light bg-light shadow">
+        <div className="container">
+          <h1 className="navbar-brand">Pt_Finder</h1>
 
-          <button className="navbar-toggler"
-           type="button"
+          <button
+            className="navbar-toggler"
+            type="button"
             data-bs-toggle="offcanvas"
-             data-bs-target="#offcanvasScrolling"
-              aria-controls="offcanvasScrolling">
-                <span className="navbar-toggler-icon"></span>
-              </button>
+            data-bs-target="#offcanvasScrolling"
+            aria-controls="offcanvasScrolling"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-          <div className="offcanvas offcanvas-start" style={{width:'50%'}} data-bs-scroll="true" data-bs-backdrop="false" tabIndex={-1} id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+          <div
+            className="offcanvas offcanvas-start"
+            style={{ width: "50%" }}
+            data-bs-scroll="true"
+            data-bs-backdrop="false"
+            tabIndex={-1}
+            id="offcanvasScrolling"
+            aria-labelledby="offcanvasScrollingLabel"
+          >
             <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasScrollingLabel">Menu</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              <h5 className="offcanvas-title" id="offcanvasScrollingLabel">
+                Menu
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
             </div>
             <div className="offcanvas-body">
-             
-            
               <ul className="navbar-nav mx-auto d-flex text-center mb-2">
-                <li className="nav-item"><a href="" className="nav-link">Home</a></li>
-                <li className="nav-item"><a href="" className="nav-link">About</a></li>
-                <li className="nav-item"><a href="" className="nav-link">Contact</a></li>
+                <li className="nav-item">
+                  <Link href="/" className="nav-link">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link href="/about" className="nav-link">
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link href="/contact" className="nav-link">
+                    Contact
+                  </Link>
+                </li>
               </ul>
               <ul className="navbar-nav ms-auto text-center">
-                <li className="nav-item"><a href="" className="btn text-light" style={{backgroundColor:'#c90ca9ff'}}>Get Started</a></li> 
-                <li className="nav-item"><a href="" className="nav-link">Sign In</a></li>
+                <li className="nav-item">
+                  <Link
+                    href="auth/signUp"
+                    className="btn text-light"
+                    style={{ backgroundColor: "#c90ca9ff" }}
+                  >
+                    Get Started
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link href="/signIn" className="nav-link">
+                    Sign In
+                  </Link>
+                </li>
               </ul>
-         
             </div>
           </div>
-      </div>
-    </nav>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
